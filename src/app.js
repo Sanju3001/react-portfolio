258eb3f5eb5762003cf8decc590d45481828ec9c
Sanju3001 <codersaj@gmail.com>
2017-11-02 01:50:11 -0400

    added client-side routing

--- src/app.js
+++ src/app.js
@@ -1,95 +1,14 @@
-// install -> import -> use
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-// const Layout = (props) => {
-//   return (
-//     <div>
-//       <p>header</p>
-//       {props.children}
-//       <p>footer</p>
-//     </div>
-//   );
-// };
-
-
-
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
 
 
 
 
-// IndecisionApp.defaultProps = {
-//   options: []
-// };
-
-
-// class Header extends React.Component {
-//   render() {
-
-//     return (
-//       <div>
-//         <h1>{this.props.title}</h1>
-//         <h2>{this.props.subtitle}</h2>
-//       </div>
-//     );
-//   }
-// }
-
-
-// class Action extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         <button
-//           onClick={this.props.pickOption}
-//           disabled={!this.props.hasOptions}
-//         >
-//           What should I do?
-//         </button>
-//       </div>
-//     );
-//   }
-// }
-
-
-// class Options extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         <button onClick={this.props.handleDeleteOptions}>Remove All</button>
-//         {
-//           this.props.options.map((option) => <Option key={option} optionText={option} />)
-//         }
-//       </div>
-//     );
-//   }
-// }
-
-
-// class Option extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         {this.props.optionText}
-//       </div>
-//     );
-//   }
-// }
-
-
-// const User = (props) => {
-//   return (
-//     <div>
-//       <p>Name: {props.name}</p>
-//       <p>Age: {props.age}</p>
-//     </div>
-//   );
-// };
 
 
 
